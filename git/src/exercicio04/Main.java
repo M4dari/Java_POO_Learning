6ea97c8c08a4e480f9c3e5790bf610af7c7dae31
Maria Eduarda Oliveira <117459982+M4dari@users.noreply.github.com>
2025-03-12 10:21:33 -0300

    Adicionando novos exercicios
    Novos exercícios de Java POO

---
+++ git/src/exercicio04/Main.java
@@ -0,0 +1,17 @@
+package exercicio04;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        Funcionario f = new Funcionario("Ana", "Estag", 4000);
+
+        double porcentagem;
+
+        System.out.println("Insira a porcentagem de aumento");
+        porcentagem = sc.nextDouble();
+
+
+    }
+}
